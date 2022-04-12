e309b45565deae09158373a1eb3fb0813f9c6729
Italo A. G <italo@eita.tech>
2022-04-11 22:44:49 -0300

    env setup

---
+++ src/main/java/tech/eita/casboot/CleanArchitectureSpringApplication.java
@@ -0,0 +1,13 @@
+package tech.eita.casboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanArchitectureSpringApplication.class, args);
+    }
+
+}
